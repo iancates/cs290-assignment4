8bfa311f9dde04ed8541a9c1f4f2be5fbd4fd06c
iancates <Ian@ians-MacBook-Pro-3.local>
2015-04-28 10:13:36 -0400

    Declared Variables

---
+++ multtable.php
@@ -0,0 +1,11 @@
+<?php
+
+$minMultiplicand = intval($_GET['min-multiplicand']);
+$maxMultiplicand = intval($_GET['max-multiplicand']);
+$minMultiplier = intval($_GET['min-multiplier']);
+$maxMultiplier = intval($_GET['max-multiplier']);
+
+echo $minMultiplier;
+    
+
+?>
